79f0a76ca49bc8bb5d8315925a7b615f4511a481
dzvezdanovic <darko.zvezdanovic@fazi.rs>
2024-02-27 08:52:25 +0100

    Chanegd logic, and response model.

---
+++ DiffService/src/models/DiffResult.cs
@@ -0,0 +1,10 @@
+﻿namespace DiffService.src.models
+{
+    public class DiffResult
+    {
+        public bool IsSuccess { get; set; }
+        public string MessageType { get; set; }
+        public string Message { get; set; }
+        public List<Difference> Differences { get; set;}
+    }
+}
